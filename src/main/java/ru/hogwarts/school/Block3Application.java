5b2df481d4dbe4504e5057e1741111fa7b4f31ee
pavelkrivokorytov <paxa-net@mail.ru>
2022-06-10 22:34:56 +0300

    commit

---
+++ src/main/java/ru/hogwarts/school/Block3Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Block3Application.class, args);
+    }
+
+}
